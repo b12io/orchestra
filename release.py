618371a8eaecb829a07c5be8f5c20e45d60883c5
Daniel Haas <dhaas@eecs.berkeley.edu>
2015-10-19 09:44:20 -0400

    Fixes a bug in the autorelease script and adds options for partial runs of the script.

--- release.py
+++ release.py
@@ -20,22 +20,35 @@ def main(args):
     new_version = increment_version_number(old_version, args.version_part)
 
     # Verify that our git setup is reasonable and print a loud warning.
-    verify_and_warn(old_version, new_version)
+    verify_and_warn(old_version, new_version, args)
 
     # Update orchestra/__init__.py with the new version
-    update_version('orchestra', new_version, fake=args.fake)
+    if args.no_commit:
+        print('--no-commit passed, not committing new version to Orchestra.')
+    else:
+        update_version('orchestra', new_version, fake=args.fake)
 
-    # Commit the update to the repo
-    commit_and_push(fake=args.fake)
+        # Commit the update to the repo
+        commit_and_push(fake=args.fake)
 
     # Create a new tag for the release
-    tag_str = tag_release(new_version, fake=args.fake)
+    version_text = 'current' if args.no_commit else 'new'
+    if args.no_tag:
+        print('--no-tag passed, not tagging the {} version.'.format(
+            version_text))
+        tag_str = 'v{}'.format(old_version)
+    else:
+        tag_str = tag_release(new_version, fake=args.fake)
 
     # Release the new version on pypi
-    pypi_release(tag_str, fake=args.fake)
+    if args.no_pypi:
+        print('--no-pypi passed, not releasing the {} version to PyPI.'.format(
+            version_text))
+    else:
+        pypi_release(tag_str, fake=args.fake)
 
 
-def verify_and_warn(old_version, new_version):
+def verify_and_warn(old_version, new_version, args):
     # verify that we're on the master branch
     branch_cmd = ['git', 'symbolic-ref', '--short', 'HEAD']
     cur_branch = run_command(branch_cmd).strip().lower()
@@ -51,6 +64,7 @@ def verify_and_warn(old_version, new_version):
         print('Your branch has commits not yet on origin/master. Exiting.')
         exit()
 
+    run_command(['git', 'fetch', 'origin', 'master'])
     unpulled_cmd = rev_cmd + ['..origin/master']
     unpulled_commits = run_command(unpulled_cmd).strip()
     if unpulled_commits:
@@ -64,19 +78,45 @@ def verify_and_warn(old_version, new_version):
         print('There are outstanding changes to your branch. Exiting.')
         exit()
 
-    print('WARNING: running this script will increment the Orchestra version '
-          'from {} to {}. This involves pushing a new commit to master and '
-          'releasing a new PyPI distribution, changes you CANNOT TAKE BACK. '
-          'Before proceeding, ensure that you are on the master branch, have '
-          'pulled the latest changes, and have no outstanding local changes. '
-          'THIS SCRIPT WILL FAIL if you do not have credentials to push to '
-          'the Orchestra GitHub repository or the Orchestra PyPI account.'
-          .format(old_version, new_version))
-    ack = input('ARE YOU SURE YOU WISH TO PROCEED? (y/n): ').lower()
-    while ack not in ['y', 'n']:
-        ack = input('Please respond with "y" or "n": ').lower()
-    if ack == 'n':
+    action_text = get_action_text(old_version, new_version, args)
+    if not action_text:  # Nothing to warn about.
+        return
+    print('WARNING: running this script will {}. This involves making changes '
+          'you CANNOT TAKE BACK. Before proceeding, ensure that you are on '
+          'the master branch, have pulled the latest changes, and have no '
+          'outstanding local changes. THIS SCRIPT WILL FAIL if you do not '
+          'have credentials to push to the Orchestra GitHub repository or the '
+          'Orchestra PyPI account.'.format(action_text))
+    ack = input('ARE YOU SURE YOU WISH TO PROCEED? (Type "release" to '
+                'confirm): ').lower().strip('\' "')
+    while ack in ['y', 'yes']:
+        ack = input('Please type "release" to confirm: ')
+    if ack != 'release':
         exit()
+
+
+def get_action_text(old_version, new_version, args):
+    actions = []
+    release_version = old_version if args.no_commit else new_version
+    if not args.no_commit:
+        actions.append('increment the Orchestra version from {} to {}'.format(
+            old_version, new_version))
+    if not args.no_tag:
+        actions.append('create a tag for version {}'.format(release_version))
+    if not args.no_pypi:
+        actions.append('release a PyPI distribution for version {}'.format(
+            release_version))
+    if not actions:
+        return ''
+
+    if len(actions) == 1:
+        action_text = actions[0]
+    elif len(actions) == 2:
+        action_text = ' and '.join(actions)
+    else:
+        actions[-1] = 'and ' + actions[-1]
+        action_text = ', '.join(actions)
+    return action_text
 
 
 def increment_version_number(old_version, version_part):
@@ -128,7 +168,7 @@ def update_version(package, new_version, fake=False):
 
 def commit_and_push(fake=False):
     # commit all changes
-    wrap_command(['git', 'commit', '-am', '"Version bump."'], fake)
+    wrap_command(['git', 'commit', '-am', 'Version bump.'], fake)
 
     # Push changes
     wrap_command(['git', 'push', 'origin', 'master'], fake)
@@ -138,17 +178,16 @@ def tag_release(version, fake=False):
     # Create the tag
     tag_str = 'v{}'.format(version)
     tag_msg = 'Version {} of Orchestra'.format(version)
-    tag_cmd = ['git', 'tag', '-am', '"{}"'.format(tag_msg), tag_str]
+    tag_cmd = ['git', 'tag', '-am', tag_msg, tag_str]
     wrap_command(tag_cmd, fake)
 
     # Update the stable tag
     stable_tag_msg = 'The latest stable release of Orchestra.'
-    stable_tag_cmd = ['git', 'tag', '-afm', '"{}"'.format(stable_tag_msg),
-                      'stable']
+    stable_tag_cmd = ['git', 'tag', '-afm', stable_tag_msg, 'stable']
     wrap_command(stable_tag_cmd, fake)
 
     # Push the tags
-    wrap_command(['git', 'push', '--tags'], fake)
+    wrap_command(['git', 'push', '-f', '--tags'], fake)
 
     return tag_str
 
@@ -211,6 +250,23 @@ def parse_args():
         action='store_true',
         help=('Fake the release without actually changing anything (useful '
               'for testing).'))
+    parser.add_argument(
+        '--no-commit',
+        action='store_true',
+        help=("Don\'t push a commit that updates the version number in the "
+              "codebase (i.e., release the current version. Useful for "
+              " continuing if the script ran partially before)."))
+    parser.add_argument(
+        '--no-tag',
+        action='store_true',
+        help=("Don\'t create a tag for the new version (i.e., release the "
+              "current version's tag. Useful for continuing if the script ran "
+              "partially before)."))
+    parser.add_argument(
+        '--no-pypi',
+        action='store_true',
+        help=("Don\'t create a new pypi release."))
+
     return parser.parse_args()
 
 
