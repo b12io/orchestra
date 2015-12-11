30a91c18e4e59e8722f6fe6127e7e161a3ebd19c
Joseph Botros <jrbotros@gmail.com>
2015-12-11 15:48:25 -0500

    Surface directives from multiple workflow versions

--- orchestra/static/orchestra/task/js/controllers.js
+++ orchestra/static/orchestra/task/js/controllers.js
@@ -43,7 +43,7 @@
           }
 
           var directiveTag = (window.orchestra
-            .angular_directives[data.workflow.slug]
+            .angular_directives[data.workflow.slug][data.workflow_version.slug]
             [data.step.slug]);
 
           var inject = []
