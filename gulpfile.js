d70e77ea20954a6bc7ba726747636e7bd0498bd9
Joshua Blum <jblum18@gmail.com>
2016-04-05 18:16:00 -0400

    Update gulpfile to only process scss
    * Update gulpfile to only process scss
    
    * add back .min extension to libraries
    
    * Add dist folder since we can't control static files dir
    
    * Fix paths for static files
    
    * Remove unused build files

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,23 @@
+// utils
 var gulp = require('gulp');
+var argv = require('yargs').argv;
 var cache = require('gulp-cached');
-var jscs = require('gulp-jscs');
-var jshint = require('gulp-jshint');
-var ngAnnotate = require('gulp-ng-annotate');
 var print = require('gulp-print');
 var rename = require('gulp-rename');
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
-var stylish = require('jshint-stylish');
-var uglify = require('gulp-uglify');
+var gulpif = require('gulp-if');
 var watch = require('gulp-watch');
 
-var buildDestination = 'orchestra/static/dist';
+// js
+var jscs = require('gulp-jscs');
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
+
+// scss
+var sourcemaps = require('gulp-sourcemaps');
+var sass = require('gulp-sass');
+
+// Files will be output here
+var staticBuildDestination = 'orchestra/static/dist/';
 
 var files = {
   scss: [
@@ -19,13 +25,10 @@ var files = {
     'orchestra/static/orchestra/common/components/iframe/css/iframe.scss',
     'orchestra/static/orchestra/common/components/quill/css/quill.scss',
     'orchestra/static/orchestra/common/css/orchestra.scss',
-    'orchestra/static/orchestra/common/css/registration.scss',
-    'orchestra/static/orchestra/dashboard/css/dashboard.scss',
-    'orchestra/static/orchestra/task/css/task.scss',
   ],
   all_scss: [],
-  scripts: [
-    '!' + buildDestination + '/**',
+  jslint: [
+    './gulpfile.js', // Lint ourselves!
     '!**/common/js/lib/**'
   ]
 };
@@ -36,45 +39,51 @@ var installedApps = [
 
 for (var i = 0; i < installedApps.length; i++) {
   var appName = installedApps[i];
-  files.scripts.push(appName + '/static/**/*.js');
-  files.scripts.push('!' + appName + '/static/**/*.min.js');
   files.all_scss.push(appName + '/static/**/*.scss');
+
+  // jslint
+  files.jslint.push(appName + '/static/**/*.js');
+  files.jslint.push('!' + appName + '/static/**/*.min.js');
 }
 
-
-gulp.task('default', ['build', 'watch']);
-gulp.task('build', ['scss', 'lint', 'scripts']);
-
 gulp.task('scss', function() {
-  return gulp.src(files.scss)
+  return gulp.src(files.scss, {
+      base: './'
+    })
+    .pipe(cache('scss'))
+    .pipe(gulpif(!argv.production, sourcemaps.init()))
     .pipe(sass())
-    .pipe(gulp.dest(buildDestination));
+    .pipe(gulpif(!argv.production, sourcemaps.write()))
+    .pipe(rename(function(path) {
+      // move to a css dir if it is in a scss dir
+      var dirname = path.dirname;
+      dirname = dirname.replace('/scss', '/css');
+      var chopPath = '/static/';
+      dirname = dirname.substring(dirname.indexOf(chopPath) + chopPath.length);
+      path.dirname = dirname;
+      return path;
+    }))
+    .pipe(gulp.dest(staticBuildDestination));
 });
 
-gulp.task('lint', function() {
-  return gulp.src(files.scripts)
-    .pipe(cache('lint'))
+gulp.task('jslint', function() {
+  return gulp.src(files.jslint)
+    .pipe(cache('jslint'))
     .pipe(jshint())
     .pipe(jshint.reporter(stylish))
-    .pipe(jshint.reporter('fail'))
+    .pipe(gulpif(argv.production, jshint.reporter('fail')))
     .pipe(jscs())
-    .pipe(jscs.reporter())
+    .pipe(jscs.reporter());
 });
 
-gulp.task('scripts', function() {
-  return gulp.src(files.scripts)
-    .pipe(cache('scripts'))
-    .pipe(sourcemaps.init())
-    .pipe(ngAnnotate())
-    .pipe(uglify({mangle: false}))
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest(buildDestination));
-});
+// TODO(joshblum): add css and scss linting
+gulp.task('lint', ['jslint']);
 
 gulp.task('watch', function() {
+  var all_lint_files = [].concat.apply([], [files.jslint]);
+  gulp.watch(all_lint_files, ['lint']);
   gulp.watch(files.all_scss, ['scss']);
-  gulp.watch(files.scripts, ['scripts']);
 });
+
+gulp.task('default', ['build', 'watch']);
+gulp.task('build', ['lint', 'scss']);
