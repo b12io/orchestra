682330d6f238abd61897f9de88fe061b989f457d
Joseph Botros <jrbotros@gmail.com>
2016-04-20 13:58:39 -0400

    Implement timecard UX for displaying worker time entries (#186)
    * Implement timecard UX for displaying worker time entries
    
    * Clean up orchestra.js

---
+++ orchestra/static/orchestra/common/js/orchestra.filters.js
@@ -0,0 +1,29 @@
+angular.module('orchestra.common')
+  .filter('capitalize', function() {
+    return function (input, format) {
+      if (!input) {
+        return input;
+      }
+      // Capitalize the first letter of an input
+      return input.charAt(0).toUpperCase() + input.slice(1);
+    };
+  })
+
+  // Modified from github.com/petebacondarwin/angular-toArrayFilter
+  .filter('toArray', function () {
+    return function (obj, addKey) {
+      if (!angular.isObject(obj)) return obj;
+      if ( addKey === false ) {
+        return Object.keys(obj).map(function(key) {
+          return obj[key];
+        });
+      } else {
+        return Object.keys(obj).map(function (key) {
+          var value = obj[key];
+          return angular.isObject(value) ?
+            Object.defineProperty(value, '$key', { enumerable: false, value: key}) :
+            { $key: key, $value: value };
+        });
+      }
+    };
+  });
