816bfbdfc20513a0762f9fdba69d868e78d95d7e
Joshua Blum <jblum18@gmail.com>
2016-04-26 22:07:53 -0400

    Add slack username to the worker settings form (#217)
    * Add slack username to the worker settings form
    
    * Remove unneeded conditional

--- orchestra/accounts/forms.py
+++ orchestra/accounts/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth import get_user_model
 
+from orchestra.models import Worker
+
 UserModel = get_user_model()
 
 
@@ -13,3 +15,12 @@ class UserForm(forms.ModelForm):
         fields = ('first_name', 'last_name')
         # TODO(joshblum): support change email
         exclude = ('is_staff', 'is_active', 'date_joined', 'username', 'email')
+
+
+class WorkerForm(forms.ModelForm):
+    slack_username = forms.CharField()
+
+    class Meta:
+        model = Worker
+        fields = ('slack_username',)
+        exclude = ('user', 'start_datetime')
