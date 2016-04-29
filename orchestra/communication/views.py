f0ed0b584583d93a6911ecefe2a518236e80b814
Joshua Blum <jblum18@gmail.com>
2016-04-29 14:58:39 +0000

    Basic styling for staffing request pages

--- orchestra/communication/views.py
+++ orchestra/communication/views.py
@@ -8,14 +8,17 @@ from orchestra.communication.staffing import handle_staffing_response
 @login_required
 def accept_staffing_request(request, staffing_request_id):
     worker = Worker.objects.get(user=request.user)
-    response = handle_staffing_response(worker, staffing_request_id, True)
+    response = handle_staffing_response(
+        worker, staffing_request_id, is_available=True)
     return render(request, 'communication/staffing_request_accepted.html',
-                  {'is_winner': response.is_winner})
+                  {
+                      'response': response,
+                  })
 
 
 @login_required
 def reject_staffing_request(request, staffing_request_id):
     worker = Worker.objects.get(user=request.user)
-    handle_staffing_response(worker, staffing_request_id, False)
+    handle_staffing_response(worker, staffing_request_id, is_available=False)
     return render(request, 'communication/staffing_request_rejected.html',
                   {})
