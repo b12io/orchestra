d4d35e1c9417be8e7d81e5fa637522649ed6df86
Kainar Kamalov <kainar.kamalov@gmail.com>
2016-05-11 19:28:18 +0000

    Updated the pr.

--- orchestra/cron.py
+++ orchestra/cron.py
@@ -2,6 +2,6 @@ import kronos
 from orchestra.communication.staffing import send_staffing_requests
 
 
-@kronos.register('*/1 * * * *')
+@kronos.register('* * * * *')  # run every minute
 def send_staffing_requests_periodically():
     send_staffing_requests()
