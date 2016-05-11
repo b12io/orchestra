20297c7ee2fbe95897b2e8bf85763748c40fd8a9
Kainar Kamalov <kainar.kamalov@gmail.com>
2016-05-11 17:50:36 +0000

    Rebase fixed 1.

--- orchestra/communication/tests/test_staffing.py
+++ orchestra/communication/tests/test_staffing.py
@@ -1,7 +1,6 @@
 from orchestra.bots.errors import StaffingResponseException
 from orchestra.communication.staffing import handle_staffing_response
 from orchestra.models import StaffingResponse
-from orchestra.models import Task
 from orchestra.models import TaskAssignment
 from orchestra.tests.helpers import OrchestraTestCase
 from orchestra.tests.helpers.fixtures import StaffingRequestInquiryFactory
@@ -12,16 +11,16 @@ class StaffingTestCase(OrchestraTestCase):
 
     def setUp(self):
         self.worker = WorkerFactory()
-        self.staffing_request = StaffingRequestInquiryFactory(
+        self.staffing_request_inquiry = StaffingRequestInquiryFactory(
             communication_preference__worker=self.worker,
-            task__step__is_human=True
+            request__task__step__is_human=True
         )
         super().setUp()
 
     def test_handle_staffing_response_invalid_request(self):
         old_count = StaffingResponse.objects.all().count()
 
-        # Invalid staffing_request_id
+        # Invalid staffing_request_inquiry_id
         response = handle_staffing_response(
             self.worker, None, is_available=True)
         self.assertEqual(response, None)
@@ -29,7 +28,7 @@ class StaffingTestCase(OrchestraTestCase):
 
         # Invalid worker
         response = handle_staffing_response(
-            None, self.staffing_request.id, is_available=True)
+            None, self.staffing_request_inquiry.id, is_available=True)
         self.assertEqual(response, None)
         self.assertEqual(StaffingResponse.objects.all().count(), old_count)
 
@@ -37,38 +36,38 @@ class StaffingTestCase(OrchestraTestCase):
         # Test StaffingResponse object creation
         old_count = StaffingResponse.objects.all().count()
         response = handle_staffing_response(
-            self.worker, self.staffing_request.id, is_available=True)
+            self.worker, self.staffing_request_inquiry.id,
+            is_available=True)
         self.assertTrue(response.is_winner)
         self.assertEqual(StaffingResponse.objects.all().count(), old_count + 1)
 
-        task_assignment = (TaskAssignment.objects
-                           .get(worker=self.worker,
-                                task=self.staffing_request.task))
+        task_assignment = (
+            TaskAssignment.objects
+            .get(worker=self.worker,
+                 task=self.staffing_request_inquiry.request.task))
         self.assertEquals(task_assignment.status,
                           TaskAssignment.Status.PROCESSING)
-        self.staffing_request.task.refresh_from_db()
-
-        self.assertEquals(self.staffing_request.task.status,
-                          Task.Status.PROCESSING)
+        self.staffing_request_inquiry.request.task.refresh_from_db()
 
         # Replay of same request
         response = handle_staffing_response(
-            self.worker, self.staffing_request.id, is_available=True)
+            self.worker, self.staffing_request_inquiry.id, is_available=True)
         self.assertTrue(response.is_winner)
         self.assertEqual(StaffingResponse.objects.all().count(), old_count + 1)
 
         # Change mind to `is_available=False` does not do anything
         with self.assertRaises(StaffingResponseException):
             response = handle_staffing_response(
-                self.worker, self.staffing_request.id, is_available=False)
+                self.worker, self.staffing_request_inquiry.id,
+                is_available=False)
 
-        new_staffing_request = StaffingRequestInquiryFactory(
-            task__step__is_human=True
+        new_request_inquiry = StaffingRequestInquiryFactory(
+            request__task__step__is_human=True
         )
-        new_worker = new_staffing_request.communication_preference.worker
+        new_worker = new_request_inquiry.communication_preference.worker
         old_count = StaffingResponse.objects.all().count()
         response = handle_staffing_response(
-            new_worker, new_staffing_request.id, is_available=True)
+            new_worker, new_request_inquiry.id, is_available=True)
         self.assertTrue(response.is_winner)
         self.assertEqual(StaffingResponse.objects.all().count(), old_count + 1)
 
@@ -76,30 +75,32 @@ class StaffingTestCase(OrchestraTestCase):
         # Test StaffingResponse object creation
         old_count = StaffingResponse.objects.all().count()
         response = handle_staffing_response(
-            self.worker, self.staffing_request.id, is_available=False)
+            self.worker, self.staffing_request_inquiry.id,
+            is_available=False)
         self.assertFalse(response.is_winner)
         self.assertEqual(StaffingResponse.objects.all().count(), old_count + 1)
 
         # Replay of same request
         response = handle_staffing_response(
-            self.worker, self.staffing_request.id, is_available=False)
+            self.worker, self.staffing_request_inquiry.id,
+            is_available=False)
         self.assertFalse(response.is_winner)
         self.assertEqual(StaffingResponse.objects.all().count(), old_count + 1)
 
         # Change mind to `is_available=True`
         response = handle_staffing_response(
-            self.worker, self.staffing_request.id, is_available=True)
+            self.worker, self.staffing_request_inquiry.id,
+            is_available=True)
         self.assertTrue(response.is_winner)
         self.assertEqual(StaffingResponse.objects.all().count(), old_count + 1)
 
         # Task is not available to claim
-        new_staffing_request = StaffingRequestInquiryFactory(
-            task=self.staffing_request.task,
-            required_role=self.staffing_request.required_role,
-            task__step__is_human=True)
-        new_worker = new_staffing_request.communication_preference.worker
+        new_request_inquiry = StaffingRequestInquiryFactory(
+            request=self.staffing_request_inquiry.request,
+            request__task__step__is_human=True)
+        new_worker = new_request_inquiry.communication_preference.worker
         old_count = StaffingResponse.objects.all().count()
         response = handle_staffing_response(
-            new_worker, new_staffing_request.id, is_available=True)
+            new_worker, new_request_inquiry.id, is_available=True)
         self.assertFalse(response.is_winner)
         self.assertEqual(StaffingResponse.objects.all().count(), old_count + 1)
