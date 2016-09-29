59cf6a4a99e7c7cee405c1de216b2ea2ad49dab0
Joshua Blum <jblum18@gmail.com>
2016-09-29 15:06:57 +0000

    Allow whitespace in image filenames

--- orchestra/utils/common_regex.py
+++ orchestra/utils/common_regex.py
@@ -1,4 +1,4 @@
 import re
 
-image_file_regex = re.compile('([-\w]+\.(?:jpg|jpeg|gif|png|svg))',
+image_file_regex = re.compile('([-\w\s]+\.(?:jpg|jpeg|gif|png|svg))',
                               re.IGNORECASE)
