efaff35352fcca1c61efe9045b591ab6f633de27
Joseph Botros <jrbotros@gmail.com>
2016-12-22 11:44:57 -0500

    Webpack dashboard view code

--- orchestra/static/orchestra/config.es6.js
+++ orchestra/static/orchestra/config.es6.js
@@ -1,5 +1,5 @@
-import dashboardTemplate from 'orchestra/dashboard/partials/dashboard.html'
 import taskTemplate from 'orchestra/task/task.html'
+import dashboardTemplate from 'orchestra/dashboard/dashboard.html'
 import projectManagementTemplate from 'orchestra/project_management/partials/project_management.html'
 import timecardTemplate from 'orchestra/timing/timecard/partials/timecard.html'
 
