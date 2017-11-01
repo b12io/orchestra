6cf5ed44698350dab7628869c7a6dd6ed0febf20
Kainar Kamalov <kainar.kamalov@gmail.com>
2017-11-01 11:43:16 +0000

    Updated task lifecycle.

--- orchestra/utils/task_lifecycle.py
+++ orchestra/utils/task_lifecycle.py
@@ -16,8 +16,12 @@ from orchestra.core.errors import TaskAssignmentError
 from orchestra.core.errors import TaskDependencyError
 from orchestra.core.errors import TaskStatusError
 from orchestra.core.errors import WorkerCertificationError
+from orchestra.models import CommunicationPreference
 from orchestra.models import Iteration
 from orchestra.models import Project
+from orchestra.models import StaffBotRequest
+from orchestra.models import StaffingRequestInquiry
+from orchestra.models import StaffingResponse
 from orchestra.models import Task
 from orchestra.models import TaskAssignment
 from orchestra.models import Worker
@@ -36,6 +40,39 @@ class AssignmentPolicyType(object):
     ENTRY_LEVEL = 'entry_level'
     REVIEWER = 'reviewer'
 
+
+def mark_worker_as_winner(staffbot_request, worker):
+    # Mark everyone else as non-winner
+    StaffingResponse.objects.filter(
+        request_inquiry__request=staffbot_request).update(is_winner=False)
+
+    staffing_response = StaffingResponse.objects.filter(
+        request_inquiry__request=staffbot_request,
+        request_inquiry__communication_preference__worker=worker)
+
+    if staffing_response.exists():
+        staffing_response = staffing_response.first()
+        staffing_response.is_winner =True
+        staffing_response.save()
+    else:
+        comm_pref = CommunicationPreference.objects.filter(
+            worker=worker)
+        if not comm_pref.exists():
+            logger.error('Worker {} does not have a communication '
+                         'preferences setup'.format(worker))
+            return
+        comm_pref = comm_pref.first()
+        comm_method = StaffingRequestInquiry.CommunicationMethod.SLACK.value
+
+        inquiry = StaffingRequestInquiry.objects.create(
+            request=staffbot_request,
+            communication_preference=comm_pref,
+            communication_method=comm_method)
+
+        StaffingResponse.objects.create(
+            request_inquiry=inquiry,
+            is_available=True,
+            is_winner=True)
 
 def worker_assigned_to_max_tasks(worker):
     """
@@ -236,6 +273,12 @@ def assign_task(worker_id, task_id):
     Iteration.objects.create(
         assignment=assignment,
         start_datetime=assignment.start_datetime)
+
+    staffbot_request = StaffBotRequest.objects.filter(
+        task=task, required_role_counter=required_role_counter)
+
+    if staffbot_request.exists():
+        mark_worker_as_winner(staffbot_request.first(), worker)
 
     if settings.PRODUCTION or settings.STAGING:
         add_worker_to_project_team(worker, task.project)
