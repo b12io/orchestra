8dbd1acb94d46e868e01a41e3a633c37b0a6a9cc
Adam Marcus <marcua@marcua.net>
2017-11-03 10:58:56 -0400

    Update settings.py

--- example_project/example_project/settings.py
+++ example_project/example_project/settings.py
@@ -14,7 +14,6 @@ import os
 import sys
 from .orchestra_settings import setup_orchestra
 
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
