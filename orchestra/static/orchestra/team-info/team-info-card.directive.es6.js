364045476972520b8c88d3211fe41c4b4460890c
Kanya Siangliulue <paoz@Kanyas-MacBook-Pro.local>
2018-01-23 16:50:28 -0500

    Show a list of team member with placeholder recorded time

---
+++ orchestra/static/orchestra/team-info/team-info-card.directive.es6.js
@@ -0,0 +1,34 @@
+import template from './team-info-card.html'
+
+export default function teamInfoCard (orchestraApi) {
+  'ngAnnotate'
+  return {
+    template,
+    restrict: 'E',
+    scope: {
+      projectId: '='
+    },
+    controllerAs: 'teamInfoCard',
+    bindToController: true,
+    controller: ($scope) => {
+      const teamInfoCard = $scope.teamInfoCard
+      orchestraApi.projectInformation(teamInfoCard.projectId)
+        .then(response => {
+          const {steps, tasks} = response.data
+          const humanSteps = new Set(steps.filter(step => step.is_human).map(step => step.slug))
+          teamInfoCard.assignments = []
+          for (let stepSlug of humanSteps.values()) {
+            const task = tasks[stepSlug]
+            if (task) {
+              teamInfoCard.assignments = teamInfoCard.assignments.concat(task.assignments.map(a => {
+                return {
+                  role: stepSlug,
+                  worker: a.worker
+                }
+              }))
+            }
+          }
+        })
+    }
+  }
+}
