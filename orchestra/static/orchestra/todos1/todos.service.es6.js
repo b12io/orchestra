677fe15016b5aea2a6220c22f21ee75c83c2544c
userify-Aditya Bharadwaj <adbharadwaj@ip-172-31-51-240.ec2.internal>
2018-06-13 13:46:05 +0000

    [WIP] Create a 2-pane interface that allows you to skip a todo

---
+++ orchestra/static/orchestra/todos1/todos.service.es6.js
@@ -0,0 +1,20 @@
+export default function todoApi ($http) {
+  const apiBase = '/orchestra/todos/todo/'
+
+  const listCreate = (projectId) => {
+    if (projectId === undefined) {
+      return apiBase
+    }
+    return `${apiBase}?project=${projectId}`
+  }
+
+  const details = (todoId) => `${apiBase}${todoId}/`
+
+  return {
+    create: (todo) => $http.post(listCreate(), todo)
+      .then(response => response.data),
+    list: (projectId) => $http.get(listCreate(projectId))
+      .then(response => response.data),
+    update: (todo) => $http.put(details(todo.id), todo)
+  }
+};
