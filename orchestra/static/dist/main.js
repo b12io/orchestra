310b1b9144346fd7fb0bbd30f150bd333533c281
userify-Aditya Bharadwaj <adbharadwaj@ip-172-31-51-240.ec2.internal>
2018-06-14 21:41:36 +0000

    [WIP] refactored code

--- orchestra/static/dist/main.js
+++ orchestra/static/dist/main.js
@@ -61584,16 +61584,9 @@ function todoApi($http) {
   var details = function details(todoId) {
     return '' + apiBase + todoId + '/';
   };
-  var response1 = {
+  var dummyResponse = {
     'data': [{ 'id': 2410, 'created_at': '2018-06-11T21:43:38.681252Z', 'task': 14231, 'description': 'Guideline A', 'completed': false, 'skipped': true, 'start_by_datetime': null, 'due_datetime': null }, { 'id': 2411, 'created_at': '2018-06-11T21:43:38.681252Z', 'task': 14231, 'description': 'Guideline B', 'completed': false, 'skipped': true, 'start_by_datetime': null, 'due_datetime': null }, { 'id': 2412, 'created_at': '2018-06-11T21:43:38.681252Z', 'task': 14231, 'description': 'Guideline C', 'completed': false, 'skipped': true, 'start_by_datetime': null, 'due_datetime': null }, { 'id': 2413, 'created_at': '2018-06-11T21:43:38.681252Z', 'task': 14231, 'description': 'Guideline D', 'completed': false, 'skipped': false, 'start_by_datetime': null, 'due_datetime': null }, { 'id': 2414, 'created_at': '2018-06-11T21:43:38.681252Z', 'task': 14231, 'description': 'Guideline E', 'completed': false, 'skipped': false, 'start_by_datetime': null, 'due_datetime': null }, { 'id': 2415, 'created_at': '2018-06-11T21:43:38.681252Z', 'task': 14231, 'description': 'Guideline F', 'completed': false, 'skipped': false, 'start_by_datetime': null, 'due_datetime': null }]
   };
-  // return {
-  //   create: (todo) => $http.post(listCreate(), todo)
-  //     .then(response => response.data),
-  //   list: (projectId) => $http.get(listCreate(projectId))
-  //     .then(response => response.data),
-  //   update: (todo) => $http.put(details(todo.id), todo)
-  // }
   return {
     create: function create(todo) {
       return $http.post(listCreate(), todo).then(function (response) {
@@ -61602,7 +61595,7 @@ function todoApi($http) {
     },
     list: function list(projectId) {
       return $http.get(listCreate(projectId)).then(function (response) {
-        return response1.data;
+        return dummyResponse.data;
       });
     },
     update: function update(todo) {
