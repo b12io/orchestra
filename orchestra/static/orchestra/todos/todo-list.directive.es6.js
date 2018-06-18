262492a218e96ae72b3970e0c4ab61b942164282
userify-Aditya Bharadwaj <adbharadwaj@ip-172-31-51-240.ec2.internal>
2018-06-18 21:59:04 +0000

    Removed unused dummy response data

--- orchestra/static/orchestra/todos/todo-list.directive.es6.js
+++ orchestra/static/orchestra/todos/todo-list.directive.es6.js
@@ -24,64 +24,6 @@ export default function todoList (orchestraApi) {
       todoList.ready = false
       todoList.taskSlugs = {}
       todoList.todos = []
-      todoList.list = [{
-        'id': 1,
-        'description': 'THINGS NOT TO DO',
-        'completed': false,
-        'items': [
-          {
-            'id': 11,
-            'description': 'node1.1',
-            'completed': false,
-            'items': [
-              {
-                'id': 111,
-                'description': 'node1.1.1',
-                'completed': false,
-                'items': []
-              }
-            ]
-          },
-          {
-            'id': 12,
-            'description': 'node1.2',
-            'completed': false,
-            'items': []
-          }
-        ]
-      },
-      {
-        'id': 2,
-        'description': 'GETTING STARTED',
-        'completed': false,
-        'items': [
-          {
-            'id': 21,
-            'description': 'node2.1',
-            'completed': false,
-            'items': []
-          },
-          {
-            'id': 22,
-            'description': 'node2.2',
-            'completed': false,
-            'items': []
-          }
-        ]
-      },
-      {
-        'id': 3,
-        'description': 'ADD/UPDATE CONTENT',
-        'completed': false,
-        'items': [
-          {
-            'id': 31,
-            'description': 'node3.1',
-            'completed': false,
-            'items': []
-          }
-        ]
-      }]
 
       const createTodo = (taskId, description, completed, startDate, dueDate) => todoApi.create({
         task: taskId,
