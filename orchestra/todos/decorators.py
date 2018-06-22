7a44de68f24dd86990db2db71ca36b9b0661f60f
userify-Aditya Bharadwaj <adbharadwaj@ip-172-31-51-240.ec2.internal>
2018-06-22 14:00:08 +0000

    Added login required auth check

--- orchestra/todos/decorators.py
+++ orchestra/todos/decorators.py
@@ -3,7 +3,7 @@ import logging
 from django.views.decorators.csrf import csrf_exempt
 from jsonview.decorators import json_view
 from rest_framework.decorators import api_view
-# from rest_framework.decorators import authentication_classes
+from django.contrib.auth.decorators import login_required
 from rest_framework.decorators import permission_classes
 from orchestra.todos.auth import IsAssociatedWithProject
 
@@ -24,6 +24,7 @@ def api_endpoint(methods):
     def api_endpoint_decorator(func):
         @csrf_exempt
         @api_view(methods)
+        @login_required
         @permission_classes((IsAssociatedWithProject,))
         @json_view
         @api_exception_logger
