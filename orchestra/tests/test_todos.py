0504e51114a133853a8414f6c6cee4f1abee8e6d
userify-Aditya Bharadwaj <adbharadwaj@ip-172-31-51-240.ec2.internal>
2018-06-22 18:59:23 +0000

    Fixed the class name for Todos test class

--- orchestra/tests/test_todos.py
+++ orchestra/tests/test_todos.py
@@ -33,7 +33,7 @@ def _todo_data(task, description, completed,
     }
 
 
-class TimeEntriesEndpointTests(EndpointTestCase):
+class TodosEndpointTests(EndpointTestCase):
 
     def setUp(self):
         super().setUp()
