6cebca320cfd4e28e9233cfcf0f177395edc78e5
ksiangliulue@gmail.com <paoz@Kanyas-MacBook-Pro.local>
2018-06-27 14:21:36 -0400

    Add a mechanism to filter out some todos when adding todos from a checklist template

--- orchestra/json_schemas/todos.py
+++ orchestra/json_schemas/todos.py
@@ -1,10 +1,23 @@
 import jsl
 
 
+class Predicate(jsl.Document):
+    operator = jsl.StringField(required=True, pattern='!?=')
+    value = jsl.AnyOfField([
+        jsl.NumberField(),
+        jsl.BooleanField(),
+        jsl.StringField(),
+        jsl.NullField()], required=True)
+
+
 class TodoSchema(jsl.Document):
     id = jsl.IntField(required=True)
     description = jsl.StringField(required=True)
     items = jsl.ArrayField(jsl.DocumentField('TodoSchema'))
+    skip_if = jsl.ArrayField(
+        jsl.DictField(jsl.DocumentField(Predicate)))
+    remove_if = jsl.ArrayField(
+        jsl.DictField(jsl.DocumentField(Predicate)))
 
 
 class TodoListSchema(jsl.Document):
--- orchestra/todos/api.py
+++ orchestra/todos/api.py
@@ -1,4 +1,6 @@
 import logging
+from pydoc import locate
+
 from orchestra.models import TodoListTemplate
 from orchestra.models import Todo
 from orchestra.models import Task
@@ -9,6 +11,18 @@ logger = logging.getLogger(__name__)
 def add_todolist_template(todolist_template_slug, task_id):
     todolist_template = TodoListTemplate.objects.get(
         slug=todolist_template_slug)
+    cond_props = {}
+    path = todolist_template.conditional_property_function.get(
+        'path', None)
+    if path:
+        try:
+            get_cond_props = locate(path)
+            kwargs = todolist_template.conditional_property_function.get(
+                'kwargs', {})
+            cond_props = get_cond_props(**kwargs)
+        except Exception:
+            logger.exception('Invalid condition function path.')
+
     task = Task.objects.get(id=task_id)
     template_todos = todolist_template.todos.get('items', [])
     root_todo = Todo(
@@ -18,16 +32,36 @@ def add_todolist_template(todolist_template_slug, task_id):
     )
     root_todo.save()
     for template_todo in template_todos:
-        _add_template_todo(template_todo, todolist_template, root_todo, task)
+        _add_template_todo(
+            template_todo, todolist_template, root_todo, task, cond_props)
 
 
-def _add_template_todo(template_todo, todolist_template, parent_todo, task):
-    todo = Todo(
-        task=task,
-        description=template_todo['description'],
-        template=todolist_template,
-        parent_todo=parent_todo
-    )
-    todo.save()
-    for template_todo_item in template_todo.get('items', []):
-        _add_template_todo(template_todo_item, todolist_template, todo, task)
+def _to_exclude(props, conditions):
+    for condition in conditions:
+        for prop, predicate in condition:
+            current_value = props.get(prop)
+            compared_to_value = predicate['value']
+            if predicate['operator'] == '=':
+                return current_value == compared_to_value
+            elif predicate['operator'] == '!=':
+                return current_value != compared_to_value
+    return False
+
+
+def _add_template_todo(
+        template_todo, todolist_template,
+        parent_todo, task, conditional_props):
+    remove = _to_exclude(conditional_props, template_todo['remove_if'])
+    if not remove:
+        todo = Todo(
+            task=task,
+            description=template_todo['description'],
+            template=todolist_template,
+            parent_todo=parent_todo,
+            skipped_datetime=_to_exclude(
+                conditional_props, template_todo['skip_if'])
+        )
+        todo.save()
+        for template_todo_item in template_todo.get('items', []):
+            _add_template_todo(
+                template_todo_item, todolist_template, todo, task)
