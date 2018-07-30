95ab6748393ba0ef446b38de7ed1b71f187284f5
Noah Picard <noah_picard@brown.edu>
2018-07-30 11:00:26 -0400

    Added searching and sorting to dashboard (#471)
    * Added searching and sorting to deshboard
    
    * Updated dependency
    
    * Moved search bar location
    
    * Removed priority column
    
    * Removed priority column

--- orchestra/static/orchestra/dashboard/dashboard.module.es6.js
+++ orchestra/static/orchestra/dashboard/dashboard.module.es6.js
@@ -2,9 +2,10 @@
 
 import tasktable from './tasktable.directive.es6.js'
 import DashboardController from './dashboard.controller.es6.js'
+import 'angular-smart-table'
 
 const name = 'orchestra.dashboard'
-angular.module(name, ['orchestra.timing'])
+angular.module(name, ['orchestra.timing', 'smart-table'])
   .directive('tasktable', tasktable)
   .controller('DashboardController', DashboardController)
 
