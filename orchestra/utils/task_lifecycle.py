c23eb6126c88bfafc0b2a36a88509f6a1ceb9986
ksiangliulue@gmail.com <paoz@Kanyas-MacBook-Pro.local>
2018-08-13 11:02:44 -0400

    Add a blank line

--- orchestra/utils/task_lifecycle.py
+++ orchestra/utils/task_lifecycle.py
@@ -1037,6 +1037,7 @@ def end_project(project_id):
         notify_status_change(task, assignment_history(task))
         notify_project_status_change(project)
 
+
 def set_project_status(project_id, status):
     """
     Set the project to the given status. Raise an exception if the status
