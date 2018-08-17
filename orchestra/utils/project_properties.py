b2d08ef0e5941c2c42d370297219c2f570b0d9f7
ksiangliulue@gmail.com <ksiangliulue@gmail.com>
2018-08-17 17:33:04 -0400

    Remove incomplete_projects function

--- orchestra/utils/project_properties.py
+++ orchestra/utils/project_properties.py
@@ -6,8 +6,3 @@ def completed_projects(projects):
     Filters `projects` queryset to completed ones.
     """
     return projects.filter(status=Project.Status.COMPLETED)
-
-
-def incomplete_projects(projects):
-    return projects.filter(Q(status=Project.Status.ACTIVE) |
-                           Q(status=Project.Status.PAUSED))
