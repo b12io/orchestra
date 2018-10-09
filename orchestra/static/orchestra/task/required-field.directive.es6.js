d36411ff3f09726d8a9b994726becfe4ec85a0b7
paopow <ksiangliulue@gmail.com>
2018-10-09 15:51:02 -0400

    Add radio input support to the required field directive (#483)
    * Add radio input support to the required field directive
    
    * Add the radio error class

--- orchestra/static/orchestra/task/required-field.directive.es6.js
+++ orchestra/static/orchestra/task/required-field.directive.es6.js
@@ -11,20 +11,22 @@ export default function orchestraRequiredField ($compile, requiredFields) {
   return {
     restrict: 'EA',
     link: function (scope, elem, attrs) {
-      var field = elem.find('input')[0]
+      var fields = elem.find('input')
+      var fieldType = fields[0].getAttribute('type')
       var errorClass = elem.attr('data-error-class')
       if (!errorClass) {
-        errorClass = field.getAttribute('type') + '-error'
+        errorClass = fieldType + '-error'
       }
-      if (field &&
-          field.getAttribute('type') !== 'checkbox' &&
-          field.getAttribute('type') !== 'text') {
+      if (fields[0] &&
+          fieldType !== 'radio' &&
+          fieldType !== 'checkbox' &&
+          fieldType !== 'text') {
         console.error('Unsupported required field type.')
         return
       }
-      requiredFields.require('input-' + field.getAttribute('type'), field)
+      requiredFields.require('input-' + fieldType, fields)
       var toggleError = function () {
-        if (requiredFields.invalid.indexOf(field) >= 0) {
+        if (requiredFields.invalid.indexOf(fields) >= 0) {
           elem.addClass('required-field-error ' + errorClass)
         } else {
           elem.removeClass('required-field-error ' + errorClass)
