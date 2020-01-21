fef545bf4c3c9ad7e3a6b2ba9ac411614440627d
dastanko <dastan.kozhomuratov@gmail.com>
2020-01-21 15:00:09 +0600

    Adding deps-upgrade script

---
+++ git-merge-dep-updates.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
+branches=("$@")
+branch="upgrading-depencies-$(date -I)"
+
+git checkout -b ${branch} > /dev/null
+
+for ((i=0; i < $#; i++))
+{
+    hash=$(git log origin/master..origin/${branches[$i]} --pretty=format:'%h')
+    git cherry-pick ${hash} > /dev/null
+    echo "Picking ${hash} from branch ${branches[$i]}->${branch}"
+}
