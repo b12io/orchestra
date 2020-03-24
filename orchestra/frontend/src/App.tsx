89eb2487c7a35db2819daedb3f28d6c8e69db776
Adam Marcus <marcua@marcua.net>
2020-03-24 13:57:08 +0000

    More routes

--- orchestra/frontend/src/App.tsx
+++ orchestra/frontend/src/App.tsx
@@ -21,11 +21,28 @@ function App() {
               <Link to="/">Dashboard</Link>
             </li>
             <li>
-              <Link to="/task/5">Task</Link>
+              <Link to="/timecard">Timecard</Link>
+            </li>
+            <li>
+              <Link to="/communication/available-staffing-requests">Available tasks</Link>
+            </li>
+            <li>
+              <Link to="/project/">Project management</Link>
+            </li>
+            <li>
+              <Link to="/accounts/settings">Account settings</Link>
+            </li>
+            <li>
+              <Link to="/accounts/logout_then_login">Sign out</Link>
             </li>
           </ul>
           <Switch>
             <Route path="/task/:taskId" children={<Task />} />
+            <Route path="/project/:projectId?" children={<div>Project management</div>} />
+            <Route path="/timecard" children={<div>Timecard</div>} />
+            <Route path="/communication/available-staffing-requests" children={<div>Available tasks</div>} />
+            <Route path="/accounts/settings" children={<div>Account settings</div>} />
+            <Route path="/accounts/logout_then_login" children={<div>Sign out</div>} />
             <Route path="/">
               <Dashboard />
             </Route>
