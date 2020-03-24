2ca1c3b91eb89946d699f18f2f383cbc97ff27b7
junusheva <aikojune@gmail.com>
2020-03-24 14:42:16 +0000

    Edit file code tabulation for ProjectsList and add margins to it

--- orchestra/frontend/src/components/ProjectsList/ProjectsList.tsx
+++ orchestra/frontend/src/components/ProjectsList/ProjectsList.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import {
+  Link
+} from "react-router-dom"
 
 import './ProjectsList.scss'
 
@@ -7,111 +10,96 @@ import TableHead from '@b12/metronome/components/layout/table/TableHead.es6'
 import TableBody from '@b12/metronome/components/layout/table/TableBody.es6'
 import TableRow from '@b12/metronome/components/layout/table/TableRow.es6'
 import TableCell from '@b12/metronome/components/layout/table/TableCell.es6'
-import Grid from '@b12/metronome/components/layout/grid/Grid.es6.js'
 
 type Props = {
 
 }
 
 const ProjectList = ({}: Props) => {
-    const rowsLabels = [
-        'Status',
-        'Project and task',
-        'Next steps',
-        'Assigned',
-        'Start by',
-        'Due by'
-    ]
-    const data = [
-        {
-            status: 'Rocky Mountain Tracking',
-            project: 'Launch v4',
-            task: 'Customer success',
-            nextStep:'Kickoff call',
-            assignedDate: '2 weeks ago',
-            startBy: 'Today, 8:00 am',
-            dueBy: '-'
-        },
-        {
-            status: 'Rocky Mountain Tracking',
-            project: 'Launch v4',
-            task: 'Customer success',
-            nextStep:'Kickoff call',
-            assignedDate: '2 weeks ago',
-            startBy: 'Today, 8:00 am',
-            dueBy: '-'
-        },
-        {
-            status: 'Rocky Mountain Tracking',
-            project: 'Launch v4',
-            task: 'Customer success',
-            nextStep:'Kickoff call',
-            assignedDate: '2 weeks ago',
-            startBy: 'Today, 8:00 am',
-            dueBy: '-'
-        },
-        {
-            status: 'Rocky Mountain Tracking',
-            project: 'Launch v4',
-            task: 'Customer success',
-            nextStep:'Kickoff call',
-            assignedDate: '2 weeks ago',
-            startBy: 'Today, 8:00 am',
-            dueBy: '-'
-        },
-        {
-            status: 'Rocky Mountain Tracking',
-            project: 'Launch v4',
-            task: 'Customer success',
-            nextStep:'Kickoff call',
-            assignedDate: '2 weeks ago',
-            startBy: 'Today, 8:00 am',
-            dueBy: '-'
-        },
-        {
-            status: 'Rocky Mountain Tracking',
-            project: 'Launch v4',
-            task: 'Customer success',
-            nextStep:'Kickoff call',
-            assignedDate: '2 weeks ago',
-            startBy: 'Today, 8:00 am',
-            dueBy: '-'
-        },
-    ]
-    return (
-        <Grid justify="center">
-           <Table
-            padding='comfortable'
-            verticalAlign='middle'
-            className='projects-list'
-            cardLike
-            >
-            <TableHead>
-                <TableRow>
-                {rowsLabels.map(rowLabel => (
-                    <TableCell
-                    key={rowLabel}
-                    align='left'
-                    ><p>{rowLabel}</p></TableCell>
-                ))}
-                </TableRow>
-            </TableHead>
-            <TableBody>
-                {data.map((row, index) => (
-                <TableRow key={index}>
-                    <TableCell><p>{row.status}</p></TableCell>
-                    <TableCell><p>{row.project} / {row.task}</p></TableCell>
-                    <TableCell><p>{row.nextStep}</p></TableCell>
-                    <TableCell><p>{row.assignedDate}</p></TableCell>
-                    <TableCell><p>{row.startBy}</p></TableCell>
-                    <TableCell><p>{row.dueBy}</p></TableCell>
-                </TableRow>
-                ))}
-            </TableBody>
-            </Table>
-        </Grid>
+  const rowsLabels = [
+    'Status',
+    'Project and task',
+    'Next steps',
+    'Assigned',
+    'Start by',
+    'Due by'
+  ]
+  const data = [
+    {
+      id: 3,
+      status: 'Rocky Mountain Tracking',
+      project: 'Launch v4',
+      task: 'Customer success',
+      nextStep:'Kickoff call',
+      assignedDate: '2 weeks ago',
+      startBy: 'Today, 8:00 am',
+      dueBy: '-'
+    },
+    {
+      id: 4,
+      status: 'Rocky Mountain Tracking',
+      project: 'Launch v4',
+      task: 'Customer success',
+      nextStep:'Kickoff call',
+      assignedDate: '2 weeks ago',
+      startBy: 'Today, 8:00 am',
+      dueBy: '-'
+    },
+    {
+      id: 5,
+      status: 'Rocky Mountain Tracking',
+      project: 'Launch v4',
+      task: 'Customer success',
+      nextStep:'Kickoff call',
+      assignedDate: '2 weeks ago',
+      startBy: 'Today, 8:00 am',
+      dueBy: '-'
+    },
+    {
+      id: 6,
+      status: 'Rocky Mountain Tracking',
+      project: 'Launch v4',
+      task: 'Customer success',
+      nextStep:'Kickoff call',
+      assignedDate: '2 weeks ago',
+      startBy: 'Today, 8:00 am',
+      dueBy: '-'
+    },
+  ]
+  return (
+    <div className='projects-list__wrapper'>
+      <Table
+        padding='comfortable'
+        verticalAlign='middle'
+        className='projects-list'
+        cardLike
+      >
+        <TableHead>
+          <TableRow>
+          {rowsLabels.map(rowLabel => (
+              <TableCell
+              key={rowLabel}
+              align='left'
+              ><p>{rowLabel}</p></TableCell>
+          ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map(row => (
+            <TableRow key={row.id}>
+              <TableCell><Link to={`/task/${row.id}`}><p>{row.status}</p></Link></TableCell>
+              <TableCell><p>{row.project} / {row.task}</p></TableCell>
+              <TableCell><p>{row.nextStep}</p></TableCell>
+              <TableCell><p>{row.assignedDate}</p></TableCell>
+              <TableCell><p>{row.startBy}</p></TableCell>
+              <TableCell><p>{row.dueBy}</p></TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
 
-    )
+  )
 }
 
 export default ProjectList
