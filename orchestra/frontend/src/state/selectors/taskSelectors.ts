dcbdcbc1e5d6fdc222c6da71744b1f19859358cd
elstonayx <elston.ayx@u.nus.edu>
2020-03-24 17:41:16 +0000

    Add selectors

--- orchestra/frontend/src/state/selectors/taskSelectors.ts
+++ orchestra/frontend/src/state/selectors/taskSelectors.ts
@@ -2,10 +2,36 @@ import { createSelector } from 'reselect'
 
 import { RootState } from '../rootReducer'
 
+const ACTIVE_TASK_STATES = [
+  'just_added',
+  'in_progress',
+  'returned'
+]
+
+const PENDING_TASK_STATES = [
+  'pending_review',
+  'pending_processing'
+]
+
 export const tasksSelector = (state: RootState) => state.dashboardTasks.tasks
 
 export const getActiveTasksSelector = createSelector(
   tasksSelector,
-  tasks => tasks.filter(task => task.state === 'in_progress')
+  tasks => tasks.filter(
+    task => ACTIVE_TASK_STATES.includes(task.state) && task.should_be_active
+  ) 
+)
+
+export const getPendingTasksSelector = createSelector(
+  tasksSelector,
+  tasks => tasks.filter(
+    task => (ACTIVE_TASK_STATES.includes(task.state) && !task.should_be_active) 
+      || PENDING_TASK_STATES.includes(task.state)
+  )
+)
+
+export const getPausedTasksSelector = createSelector(
+  tasksSelector,
+  tasks => tasks.filter(task => task.state === 'paused')
 )
 
