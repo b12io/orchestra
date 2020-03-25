d201d765b90ea9627782c8239a8df94e61286607
elstonayx <elston.ayx@u.nus.edu>
2020-03-25 06:40:23 +0000

    Format file and add cmdclick handler

--- orchestra/frontend/src/components/TasksList/TasksList.tsx
+++ orchestra/frontend/src/components/TasksList/TasksList.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from 'react'
 import {
   useHistory
-} from "react-router-dom"
+} from 'react-router-dom'
 
 import './TasksList.scss'
 
@@ -18,9 +18,9 @@ import AnimatedCircle from '../../assets/AnimatedCircle'
 import { getPrettyDatetime, specialFormatIfToday } from '../../util/time'
 
 type ProjectListProps = {
-  status: any,
-  tasks: any,
-  isLoading?: boolean
+  status: any;
+  tasks: any;
+  isLoading?: boolean;
 }
 
 const TaskList = ({ status, tasks, isLoading = false }: ProjectListProps) => {
@@ -35,11 +35,11 @@ const TaskList = ({ status, tasks, isLoading = false }: ProjectListProps) => {
   // Map Orchestra tag values to Metronome badge color values.
   const tagMapping = {
     'default': 'primary',
-    'primary': 'primary',  // We'll add `selected` below to make it stand out.
+    'primary': 'primary', // We'll add `selected` below to make it stand out.
     'success': 'success',
     'info': 'neutral',
     'warning': 'warning',
-    'danger': 'warning'  // We'll add `selected` below to make it stand out.
+    'danger': 'warning' // We'll add `selected` below to make it stand out.
   }
   const history = useHistory()
 
@@ -53,8 +53,18 @@ const TaskList = ({ status, tasks, isLoading = false }: ProjectListProps) => {
         row.next_todo_dict.due_datetime,
         specialFormatIfToday(row.next_todo_dict.due_datetime))
 
+      const rowUrl = `/task/${row.id}`
+
+      const handleRowClick = (event: MouseEvent) => {
+        if (event.ctrlKey || event.metaKey) {
+          window.open('/orchestra/newapp/#' + rowUrl, '_blank')
+        } else {
+          history.push(rowUrl)
+        }
+      }
+
       return (
-        <TableRow key={row.id} onClick={() => history.push(`/task/${row.id}`)}>
+        <TableRow key={row.id} onClick={handleRowClick}>
           <TableCell>
             <h4>{row.detail}</h4>
             {row.tags.map(tag => {
@@ -64,7 +74,8 @@ const TaskList = ({ status, tasks, isLoading = false }: ProjectListProps) => {
               }
               return (
                 <Badge size="small" label={tag.label} filled className='dsu-mr-xxxsm' {...colorProps}/>
-              )})}
+              )
+            })}
           </TableCell>
           <TableCell><p>{row.project} / {row.step}</p></TableCell>
           <TableCell><p>{assigned}</p></TableCell>
@@ -72,7 +83,8 @@ const TaskList = ({ status, tasks, isLoading = false }: ProjectListProps) => {
           <TableCell><p>{startBy}</p></TableCell>
           <TableCell><p>{dueBy}</p></TableCell>
         </TableRow>
-      )})
+      )
+    })
   }
 
   const renderEmptyList = () => (
