6f03d90f9ca9ed8140243c26c8ee4ae8390d4269
elstonayx <elston.ayx@u.nus.edu>
2020-06-04 14:23:21 +0000

    WIP

--- orchestra/frontend/src/views/task/Task.tsx
+++ orchestra/frontend/src/views/task/Task.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
-import {
-  useParams
-} from 'react-router-dom'
+import { useParams } from 'react-router-dom'
+import store from '../../state/store'
+
+window.ReactOrchestra = require('react')
 
 const Task = () => {
-  const { taskId } = useParams();
+  const { taskId } = useParams()
+  const LoadableTaskComponent = window.orchestra?.task
   return (
-        <div>
-          Task view: { taskId }
-        </div>
-    )
+    <div>
+      {LoadableTaskComponent && <LoadableTaskComponent store={store} />}
+    </div>
+  )
 }
 
-export default Task;
+export default Task
