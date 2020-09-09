ecfd22aacb5a53f2f553904fa0379955a91231a9
muratjumashev <jumasheff@gmail.com>
2020-09-09 16:25:03 +0000

    Remove task from IsAssociatedWithProject

--- orchestra/todos/auth.py
+++ orchestra/todos/auth.py
@@ -14,9 +14,9 @@ class IsAssociatedWithTodosProject(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         worker = Worker.objects.get(user=request.user)
         if isinstance(obj, Todo):
-            project = obj.task.project
+            project = obj.project
         elif isinstance(obj, TodoQA):
-            project = obj.todo.task.project
+            project = obj.todo.project
         else:
             project = None
         return (
@@ -34,29 +34,10 @@ class IsAssociatedWithProject(permissions.BasePermission):
 
     def has_permission(self, request, view):
         worker = Worker.objects.get(user=request.user)
+        project_id = request.data.get('project')
         if worker.is_project_admin():
             return True
-        if request.method == 'GET':
-            # List calls have a project ID
-            project_id = request.query_params.get('project')
-            return worker.assignments.filter(task__project=project_id).exists()
-        elif request.method == 'POST':
-            # Create calls have a task ID
-            task_id = request.data.get('task')
-            todo_id = request.data.get('todo')
-            try:
-                if task_id:
-                    task = Task.objects.get(id=task_id)
-                elif todo_id:
-                    task = Todo.objects.get(id=todo_id).task
-                else:
-                    task = None
-                return task and \
-                    worker.assignments.filter(
-                        task__project=task.project).exists()
-            except (Task.DoesNotExist, Todo.DoesNotExist):
-                return False
-        return False
+        return worker.assignments.filter(task__project__id=project_id).exists()
 
 
 class IsAssociatedWithTask(permissions.BasePermission):
