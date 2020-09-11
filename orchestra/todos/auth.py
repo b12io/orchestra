170d2c784bef918ca647d780a988de46a32e2eb1
muratjumashev <jumasheff@gmail.com>
2020-09-11 06:00:57 +0000

    Fix todos/auth

--- orchestra/todos/auth.py
+++ orchestra/todos/auth.py
@@ -33,16 +33,16 @@ class IsAssociatedWithProject(permissions.BasePermission):
 
     def has_permission(self, request, view):
         worker = Worker.objects.get(user=request.user)
+        if worker.is_project_admin():
+            return True
         todo_id = request.data.get('todo')
         project_id = None
         if todo_id is not None:
-            project_id = Todo.objects.get(id=todo_id).project_id
+            project_id = Todo.objects.get(id=todo_id).project.id
         if project_id is None:
             project_id = request.query_params.get('project')
         if project_id is None:
             project_id = request.data.get('project')
-        if worker.is_project_admin():
-            return True
         return worker.assignments.filter(task__project__id=project_id).exists()
 
 
