1c432fca1f2efe345da98ef7bc1151e29845c4bb
muratjumashev <jumasheff@gmail.com>
2020-09-14 10:01:45 +0000

    Remove console.log

--- orchestra/static/orchestra/todos/todolist-template.service.es6.js
+++ orchestra/static/orchestra/todos/todolist-template.service.es6.js
@@ -10,10 +10,7 @@ export default function todoListTemplateApi ($http) {
   }
 
   return {
-    updateTodoListFromTemplate: (data) => {
-      console.log(data)
-      return $http.post(updateTodoListFromTemplate(), data).then(response => response.data)
-    },
+    updateTodoListFromTemplate: (data) => $http.post(updateTodoListFromTemplate(), data).then(response => response.data),
     list: (projectId) => $http.get(list())
       .then(response => response.data)
   }
