4f8df18fd09b36f159ecb6ef772eae9feec408ee
Aditya Bharadwaj <aditya@b12.io>
2020-10-01 14:43:23 +0000

    parallelized the api requests

--- orchestra/static/orchestra/todos/todo-list.directive.es6.js
+++ orchestra/static/orchestra/todos/todo-list.directive.es6.js
@@ -154,16 +154,14 @@ export default function todoList (orchestraApi) {
             }, {})
           todoList.possibleTasks = Object.values(tasks).filter(task => task.status !== 'Complete' && humanSteps.has(task.step_slug))
 
-          // TODO(marcua): parallelize requests rather than chaining `then`s.
-          todoApi.list(todoList.projectId).then((todos) => {
-            todoListTemplateApi.list().then((templates) => {
-              todoQaApi.workerTaskRecentTodoQas(todoList.taskId).then((todoQas) => {
-                todoList.todoQas = todoQas
-                todoList.templates = templates
-                todoList.todos = todoList.transformToTree(todos)
-                todoList.ready = true
-              })
-            })
+          const p1 = todoListTemplateApi.list()
+          const p2 = todoQaApi.workerTaskRecentTodoQas(todoList.taskId)
+          const p3 = todoApi.list(todoList.projectId)
+          Promise.all([p1, p2, p3]).then((values) => {
+            todoList.todoQas = values[1]
+            todoList.templates = values[0]
+            todoList.todos = todoList.transformToTree(values[2])
+            todoList.ready = true
           })
         })
     }
