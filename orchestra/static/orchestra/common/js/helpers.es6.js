10d0893af91db9b4d7eeac00098862980d50d89b
jumasheff <jumasheff@gmail.com>
2020-10-22 20:03:44 +0600

    Team info card: show unassigned (#695)
    * Hide show unassigned tasks
    
    * Simplify orchestra_api interfaces
    
    * Implement explicit assignment
    
    * Add reassignment logic
    
    * Implement reassignment
    
    * Fix Staff button
    
    * Display input and button in separate lines
    
    * Disabled btn after sending request
    
    * Show inputs by default
    
    * Do not show hide/show unassigned to non-admins
    
    * Do not show input if not staffable
    
    * Fixes
    
    * Add assignments sorting
    
    * Sort unassigned tasks
    
    * Clean up
    
    * Rename

--- orchestra/static/orchestra/common/js/helpers.es6.js
+++ orchestra/static/orchestra/common/js/helpers.es6.js
@@ -1,7 +1,26 @@
+const d3 = require('d3')
+
 export default function helpers () {
   return {
     isTaskStaffable: function (taskStatus) {
       return taskStatus !== 'Complete' && taskStatus !== 'Aborted'
+    },
+
+    getSortedTasksSlugs: function (tasks) {
+      return Object.keys(tasks).sort((a, b) => {
+        const previousTask = tasks[a]
+        const nextTask = tasks[b]
+        return d3.ascending(
+          new Date(previousTask.start_datetime),
+          new Date(nextTask.start_datetime)
+        )
+      })
+    },
+
+    orderAssigmentsUsingListOfSlugs: function (orderedListOfSlugs, assignmentDataList) {
+      assignmentDataList.sort((a, b) => {
+        return orderedListOfSlugs.indexOf(a.stepSlug) - orderedListOfSlugs.indexOf(b.stepSlug)
+      })
     }
   }
 }
