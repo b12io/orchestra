1375447daac1e5c7445a626466babc23803e16df
muratjumashev <jumasheff@gmail.com>
2020-11-04 18:45:07 +0000

    Rename filter backend

--- orchestra/todos/filters.py
+++ orchestra/todos/filters.py
@@ -1,7 +1,11 @@
 from rest_framework import filters
 
 
-class TodoFilterBackend(filters.BaseFilterBackend):
+class QueryParamsFilterBackend(filters.BaseFilterBackend):
+    """
+    Takes queryparams from a URL, serializes into a python data structure
+    and passes it as a queryset arguments
+    """
     def filter_queryset(self, request, queryset, view):
         params = request.query_params.dict()
         serializer = view.get_serializer(data=params, **view.kwargs)
