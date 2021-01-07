6d014e03dd23837ca8ef365c63bf62772c0b51b9
Aditya Bharadwaj <adbharadwaj@users.noreply.github.com>
2021-01-07 09:10:24 -0800

    Remove unused Kronos dependency (#734)
    (cherry picked from commit a9d03e6659a0a0633d3c3621730348ba67328217)
    
    Co-authored-by: Adam Marcus <marcua@marcua.net>

--- orchestra/cron.py
+++
@@ -1,8 +0,0 @@
-import kronos
-
-from orchestra.communication.staffing import send_staffing_requests
-
-
-@kronos.register('* * * * *')  # run every minute
-def send_staffing_requests_periodically():
-    send_staffing_requests()
