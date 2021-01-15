91f596fa55ab929500cb1e42775e31ed445dc90c
Aditya Bharadwaj <aditya@b12.io>
2021-01-15 20:16:10 +0000

    get todo templates

--- orchestra/orchestra_api.py
+++ orchestra/orchestra_api.py
@@ -151,3 +151,8 @@ def message_project_team(project_id, message):
     response = _make_api_request('post', 'message_project_team',
                                  data=json.dumps(data))
     return json.loads(response.text)
+
+
+def get_todo_templates():
+    response = _make_api_request('get', 'todo_templates')
+    return json.loads(response.text)
