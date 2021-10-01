f2ac9880edfa47f7e19ee5817e80b60aa88ae7b5
muratjumashev <jumasheff@gmail.com>
2021-10-01 10:48:07 +0000

    Linting fix

--- orchestra/utils/common_regex.py
+++ orchestra/utils/common_regex.py
@@ -1,4 +1,4 @@
 import re
 
-image_file_regex = re.compile('([-\w\s]+\.(?:jpg|jpeg|gif|png|svg))',
+image_file_regex = re.compile('([-\w\s]+\.(?:jpg|jpeg|gif|png|svg))',  # noqa
                               re.IGNORECASE)
