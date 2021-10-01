fd23254ed42614d829e6f964c5bff92f049f3d89
muratjumashev <jumasheff@gmail.com>
2021-10-01 16:15:26 +0000

    Remove noqa

--- orchestra/utils/common_regex.py
+++ orchestra/utils/common_regex.py
@@ -1,4 +1,4 @@
 import re
 
-image_file_regex = re.compile('([-\w\s]+\.(?:jpg|jpeg|gif|png|svg))',  # noqa
+image_file_regex = re.compile(r'([-\w\s]+\.(?:jpg|jpeg|gif|png|svg))',
                               re.IGNORECASE)
