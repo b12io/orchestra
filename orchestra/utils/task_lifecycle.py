1ba58d26c508e89d2050dc9b0c8439eb1eadd014
Elston Aw <elston.ayx@u.nus.edu>
2021-12-15 16:23:43 +0800

    Update tasktable to show next steps in paused tasks (#816)

--- orchestra/utils/task_lifecycle.py
+++ orchestra/utils/task_lifecycle.py
@@ -549,7 +549,7 @@ def tasks_assigned_to_worker(worker):
             next_todo_title = None
             next_todo_dict = {}
             should_be_active = False
-            if state in ('returned', 'in_progress'):
+            if state in ('returned', 'in_progress', 'paused'):
                 # TODO(aditya): Temporarily we are filtering out todos
                 # with section values. Remove this comment once we
                 # figure out a long term logic.
@@ -622,8 +622,9 @@ def tasks_assigned_to_worker(worker):
                     )
                 )
                 should_be_active = (
-                    (num_non_template_todos == 0)
-                    or task_started)
+                    state != 'paused' and
+                    ((num_non_template_todos == 0)
+                     or task_started))
             tasks_assigned.append({
                 'id': task_assignment.task.id,
                 'assignment_id': task_assignment.id,
