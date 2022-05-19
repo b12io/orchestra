790fe5bca0092c0622f9cc1bccf166aa5a99868f
muratjumashev <jumasheff@gmail.com>
2022-05-19 14:20:46 +0000

    Linting fixes

--- orchestra/orchestra_api.py
+++ orchestra/orchestra_api.py
@@ -128,7 +128,7 @@ def update_todos(updated_todos):
 
 
 def delete_todos(todo_ids):
-    headers = { 'Content-Type': 'application/json; charset=UTF-8' }
+    headers = {'Content-Type': 'application/json; charset=UTF-8'}
     response = _make_api_request('delete', 'todo-api',
                                  headers=headers,
                                  data=json.dumps(todo_ids))
