83987feab7f5d40812f2d73cffafb146c0fc55ae
Elaman Bazarov <elaman.bazarov98@gmail.com>
2025-08-08 15:00:49 +0000

    flake8 fix remaining files

--- orchestra/utils/s3.py
+++ orchestra/utils/s3.py
@@ -40,7 +40,10 @@ def upload_file(bucket_name, data, mime_type, extension,
     Upload file to provided s3 bucket and set file properties and permission.
     Returns a URL to the file.
     """
-    s3_client = boto3.client('s3', aws_access_key_id=settings.AWS_S3_KEY, aws_secret_access_key=settings.AWS_S3_SECRET)
+    s3_client = boto3.client(
+        's3',
+        aws_access_key_id=settings.AWS_S3_KEY,
+        aws_secret_access_key=settings.AWS_S3_SECRET)
 
     s3_file_name = '{}.{}'.format(uuid1().hex, extension)
     if len(data) > MAX_UPLOAD_SIZE_MB * 10 ** 6:
@@ -49,16 +52,16 @@ def upload_file(bucket_name, data, mime_type, extension,
 
     if prefix is not None:
         s3_file_name = os.path.join(prefix, s3_file_name)
-    
+
     extra_args = {'ContentType': mime_type}
     if acl_string is not None:
         extra_args['ACL'] = acl_string
-    
+
     s3_client.put_object(
         Bucket=bucket_name,
         Key=s3_file_name,
         Body=data,
         **extra_args
     )
-    
+
     return f"https://{bucket_name}.s3.amazonaws.com/{s3_file_name}"
